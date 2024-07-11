7a11410fa1db1d944adc772d9c4f686c5e2f9538
ImShad0w <kirill19042006@gmail.com>
2024-07-11 23:01:52 +0200

    Added sticky header, again

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('main-header');
+  if (window.scrollY > 50) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
